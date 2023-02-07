2c7c1752c44965fa0b1f25ab803553faaf71d8c5
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-07 15:46:30 +0100

    EDIT completata funzione JS

--- js/app.js
+++ js/app.js
@@ -4,3 +4,25 @@ console.log ('While VS For')
 
 let dispari = [];
 let conto = 0;
+
+//2. Ciclo while con richiesta utente di inserimento numero
+
+while (conto < 10) {
+    let userNumber = prompt(" inserisci un numero:");
+    userNumber = parseInt(userNumber);
+
+    //SE numero pari o undefined uso continue per continuare il ciclo while
+      //ALTRIMENTI se è dispari se il resto x 2 non è guale a 0, lo incrementa insieme ai num dispari
+      if (userNumber % 2 !== 0) {
+        dispari.push(userNumber);
+        conto++;
+      }
+    } 
+    
+    
+//3. SE ottenuti 10 numeri dispari, stampo in console uno alla volta
+
+console.log("I numeri dispari che hai inserito sono:");
+for (let i = 0; i < dispari.length; i++) {
+  console.log(dispari[i]);
+}
